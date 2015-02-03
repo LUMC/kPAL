b8138d6554bbf43c5bf3217f301b7f53b5c3bb16
Martijn Vermaat <martijn@vermaat.name>
2015-02-03 13:33:12 +0100

    Minimal version for h5py dependency is 2.1
    An older h5py version was supposedly the cause of #2 since storing
    unicode strings in attributes is only supported from h5py 2.1 and
    up.
    
    http://docs.h5py.org/en/latest/whatsnew/2.1.html#unicode-strings-allowed-in-attributes

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import sys
 if sys.version_info < (2, 6):
     raise Exception('kPAL requires Python 2.6 or higher.')
 
-dependencies = ['numpy', 'h5py', 'biopython', 'semantic-version', 'future']
+dependencies = ['numpy', 'h5py>=2.1', 'biopython', 'semantic-version',
+                'future']
 if sys.version_info[:2] == (2, 6):
     dependencies.extend(['argparse', 'importlib', 'unittest2'])
 
